6e25814a6163fc445416ae912da7e4001d80f10b
Mudassir Ijaz <ijazmudassir786@gmail.com>
2020-02-25 13:15:32 +0500

    material ui applied

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Sign_Up from "./Conponents/Signup";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Sign_Up />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
