ac7fb203802e6843835d357fdb261bbd87957d55
Mudassir Ijaz <ijazmudassir786@gmail.com>
2020-02-28 16:20:21 +0500

    dropdown

--- src/Conponents/AddService.jsx
+++ src/Conponents/AddService.jsx
@@ -3,192 +3,214 @@ import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import Joi from "joi-browser";
 import axios from "axios";
-
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
 class Add_Service extends Component {
-	state = {
-		Service: {
-			service_id: "",
-			service_name: "half cut",
-			category_name: "",
-			Salon_id: "",
-			price: 20,
-			img_url: "",
-			service_description:
-				"this is the haircut performed by xyz jkkjjkfd knf dfkkdf kdkd"
-		},
-		Category: [],
-		error: {}
-	};
-	constructor() {
-		super();
+  state = {
+    Service: {
+      service_id: "",
+      service_name: "half cut",
+      category_name: "",
+      Salon_id: "",
+      price: 20,
+      img_url: "",
+      service_description:
+        "this is the haircut performed by xyz jkkjjkfd knf dfkkdf kdkd"
+    },
+    category: ["Hair", "Facial", "Khat"],
+    value: "",
+    error: {}
+  };
+  constructor() {
+    super();
 
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-		this.validate = this.validate.bind(this);
-		this.validate_single = this.validate_single.bind(this);
-		this.handlefilechange = this.handlefilechange.bind(this);
-	}
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.validate = this.validate.bind(this);
+    this.validate_single = this.validate_single.bind(this);
+    this.handlefilechange = this.handlefilechange.bind(this);
+  }
 
-	schema = {
-		service_name: Joi.string()
-			.required()
-			.min(2)
-			.label("Service Name"),
-		// category_name: Joi.string()
-		// 	.required()
-		// 	.min(5)
-		// 	.label("Category"),
-		price: Joi.number()
-			.required()
-			.min(1)
-			.label("confirm_NewPassword"),
+  schema = {
+    service_name: Joi.string()
+      .required()
+      .min(2)
+      .label("Service Name"),
+    // category_name: Joi.string()
+    // 	.required()
+    // 	.min(5)
+    // 	.label("Category"),
+    price: Joi.number()
+      .required()
+      .min(1)
+      .label("confirm_NewPassword"),
 
-		img_url: Joi.required()
-	};
+    img_url: Joi.required()
+  };
 
-	validate() {
-		const { error } = Joi.validate(this.state.Service, this.schema, {
-			abortEarly: false
-		});
-		if (!error) return null;
-		const errors = {};
+  validate() {
+    const { error } = Joi.validate(this.state.Service, this.schema, {
+      abortEarly: false
+    });
+    if (!error) return null;
+    const errors = {};
 
-		error.details.map(item => {
-			errors[item.path[0]] = item.message;
-		});
-		return errors;
-	}
-	validate_single(field_name) {
-		console.log(this.state.Service[field_name]);
-		const { error } = Joi.validate(
-			this.state.Service[field_name],
-			this.schema[field_name]
-		);
-		if (!error) return null;
-		return error;
-	}
-	async handleSubmit(e) {
-		console.log("state is ", this.state.Service);
+    error.details.map(item => {
+      errors[item.path[0]] = item.message;
+    });
+    return errors;
+  }
+  validate_single(field_name) {
+    console.log(this.state.Service[field_name]);
+    const { error } = Joi.validate(
+      this.state.Service[field_name],
+      this.schema[field_name]
+    );
+    if (!error) return null;
+    return error;
+  }
+  async handleSubmit(e) {
+    console.log("state is ", this.state.Service);
 
-		let form_data = new FormData();
-		form_data.append("image", this.state.Service.img_url);
-		form_data.append("servicename", this.state.Service.service_name);
-		form_data.append("price", this.state.Service.price);
-		form_data.append("description", this.state.Service.service_description);
-		const error = this.validate();
-		this.setState({ error: error || {} });
-		console.log("form data is ", form_data);
-		axios({
-			url:
-				//"https://digital-salon-app.herokuapp.com/Digital_Saloon.com/api/salonservices",
-				"http://localhost:5000/Digital_Saloon.com/api/salonservices",
-			method: "POST",
-			data: form_data
-		})
-			.then(function(response) {
-				console.log(response);
-			})
-			.catch(function(error) {
-				alert(error);
-			});
-	}
-	handleChange = e => {
-		const { name, value } = e.target;
+    let form_data = new FormData();
+    form_data.append("image", this.state.Service.img_url);
+    form_data.append("servicename", this.state.Service.service_name);
+    form_data.append("price", this.state.Service.price);
+    form_data.append("description", this.state.Service.service_description);
+    const error = this.validate();
+    this.setState({ error: error || {} });
+    console.log("form data is ", form_data);
+    axios({
+      url:
+        "https://digital-salon-app.herokuapp.com/Digital_Saloon.com/api/salonservices",
+      // "http://localhost:5000/Digital_Saloon.com/api/salonservices",
+      method: "POST",
+      data: form_data
+    })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        alert(error);
+      });
+  }
+  click = e => {
+    const Service = { ...this.state.Service };
+    Service.category_name = e.target.value;
+    this.setState({ Service });
+  };
+  handleChange = e => {
+    const { name, value } = e.target;
 
-		const Service = { ...this.state.Service };
-		Service[name] = value;
-		this.setState({ Service });
-		const error = this.validate_single(name);
-		const state_error = { ...this.state.error };
-		if (error) state_error[name] = error.message;
-		else delete state_error[name];
-		this.setState({ error: state_error });
+    const Service = { ...this.state.Service };
+    Service[name] = value;
+    this.setState({ Service });
+    const error = this.validate_single(name);
+    const state_error = { ...this.state.error };
+    if (error) state_error[name] = error.message;
+    else delete state_error[name];
+    this.setState({ error: state_error });
 
-		if (error) return;
-	};
+    if (error) return;
+  };
 
-	handlefilechange = e => {
-		//console.log("h", e.target.files[0].name);s
-		//if (e) {
-		//	console.log("name", e.target.name);
-		//console.log("h", e.target.files[0]);
-		//console.log("event", e.target.files[0]);
-		if (e.target.files[0]) {
-			const size = e.target.files[0].size / 1024;
+  handlefilechange = e => {
+    //console.log("h", e.target.files[0].name);s
+    //if (e) {
+    //	console.log("name", e.target.name);
+    //console.log("h", e.target.files[0]);
+    //console.log("event", e.target.files[0]);
+    if (e.target.files[0]) {
+      const size = e.target.files[0].size / 1024;
 
-			if (size < 10240) {
-				//console.log("img", e.target.files[0]);
-				//	this.state.Service.service_name = "ju";
-				//	sconsole.log("sates", this.state.Service);
-				//this.setState({ Service });
-				const Service = { ...this.state.Service };
-				//	console.log(Service);
-				Service["img_url"] = URL.createObjectURL(e.target.files[0]);
-				//	console.log();
-				//console.log("service is", Service);
-				//this.setState({ Service });
-				//clone = e.target.files[0];
-				//	console.log("clone is ", clone);
-				this.setState({ Service });
-				//console.log("after updating statees", this.state.Service);
-			}
-		}
-	};
-	render() {
-		return (
-			<div className="container">
-				<input
-					type="file"
-					onChange={this.handlefilechange}
-					//	value={this.state.Service.img_url}
-					//name="img_url"
-				></input>
-				<br></br>
-				<span className="p-float-label">
-					{/* <label htmlFor="service_name">Service name</label> */}
-					<InputText
-						id="service_name"
-						placeholder="xyz@gmail.com"
-						value={this.state.Service.service_name}
-						onChange={this.handleChange}
-						name="service_name"
-					/>
-					<br></br>
-					<div>{this.state.error.service_name}</div>
-				</span>
-				<span className="p-float-label">
-					{/* <label htmlFor="service_price">Service Price</label> */}
-					<InputText
-						id="service_price"
-						// placeholder="xyz@gmail.com"
-						value={this.state.Service.price}
-						onChange={this.handleChange}
-						name="price"
-					/>
-					<br></br>
-					<div>{this.state.error.price}</div>
-				</span>
+      if (size < 10240) {
+        //console.log("img", e.target.files[0]);
+        //	this.state.Service.service_name = "ju";
+        //	sconsole.log("sates", this.state.Service);
+        //this.setState({ Service });
+        const Service = { ...this.state.Service };
+        //	console.log(Service);
+        Service["img_url"] = URL.createObjectURL(e.target.files[0]);
+        //	console.log();
+        //console.log("service is", Service);
+        //this.setState({ Service });
+        //clone = e.target.files[0];
+        //	console.log("clone is ", clone);
+        this.setState({ Service });
+        //console.log("after updating statees", this.state.Service);
+      }
+    }
+  };
+  render() {
+    return (
+      <div className="container">
+        <input
+          type="file"
+          onChange={this.handlefilechange}
+          //	value={this.state.Service.img_url}
+          //name="img_url"
+        ></input>
+        <br></br>
+        <span className="p-float-label">
+          {/* <label htmlFor="service_name">Service name</label> */}
+          <InputText
+            id="service_name"
+            placeholder="xyz@gmail.com"
+            value={this.state.Service.service_name}
+            onChange={this.handleChange}
+            name="service_name"
+          />
+          <br></br>
+          <div>{this.state.error.service_name}</div>
+        </span>
+        <span className="p-float-label">
+          {/* <label htmlFor="service_price">Service Price</label> */}
+          <InputText
+            id="service_price"
+            // placeholder="xyz@gmail.com"
+            value={this.state.Service.price}
+            onChange={this.handleChange}
+            name="price"
+          />
+          <br></br>
+          <div>{this.state.error.price}</div>
+        </span>
 
-				<InputText
-					fullWidth
-					// placeholder="xyz@gmail.com"
-					value={this.state.Service.service_description}
-					onChange={this.handleChange}
-					name="price"
-				/>
-				<div>{this.state.error.price}</div>
-				<Button
-					//variant="contained"
-					//color="primary"
-					fullWidth
-					// disabled={this.validate()}
-					onClick={this.handleSubmit}
-					label="Add Service"
-					className="p-button-raised p-button-rounded"
-				/>
-			</div>
-		);
-	}
+        <InputText
+          fullWidth
+          // placeholder="xyz@gmail.com"
+          value={this.state.Service.service_description}
+          onChange={this.handleChange}
+          name="price"
+        />
+        <div>{this.state.error.price}</div>
+        <TextField
+          id="filled-select-currency"
+          select
+          label="Select"
+          value={this.state.Service.category_name}
+          onChange={this.click}
+          helperText="Please select your currency"
+          variant="filled"
+        >
+          {this.state.category.map(option => (
+            <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </TextField>
+        <Button
+          //variant="contained"
+          //color="primary"
+          fullWidth
+          // disabled={this.validate()}
+          onClick={this.handleSubmit}
+          label="Add Service"
+          className="p-button-raised p-button-rounded"
+        />
+      </div>
+    );
+  }
 }
 
 export default Add_Service;
--- src/Conponents/dashboard/CardMaterial.jsx
+++ src/Conponents/dashboard/CardMaterial.jsx
@@ -17,76 +17,85 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Axios from "axios";
+import Paper from "@material-ui/core/Paper";
 
 export default class CardMaterial extends React.Component {
-	state = {
-		List_of_services: [123],
-		name: "mudassir"
-	};
+  state = {
+    List_of_services: [123],
+    name: "mudassir"
+  };
 
-	componentDidMount() {
-		Axios({
-			url:
-				"https://digital-salon-app.herokuapp.com/Digital_Saloon.com/api/salonservices",
-			method: "GET"
-		})
-			.then(response => {
-				console.log("RESPONSE OBJECT", response.data);
-				// List_of_services = { ...this.state.List_of_services0 };
-				// List_of_services = response;
-				this.setState({ List_of_services: response.data });
-			})
+  componentDidMount() {
+    Axios({
+      url:
+        "https://digital-salon-app.herokuapp.com/Digital_Saloon.com/api/salonservices",
+      method: "GET"
+    })
+      .then(response => {
+        console.log("RESPONSE OBJECT", response.data);
+        // List_of_services = { ...this.state.List_of_services0 };
+        // List_of_services = response;
+        this.setState({ List_of_services: response.data });
+      })
 
-			// console.log("IN COMPONENT DID MOUNT", this.state.name);
+      // console.log("IN COMPONENT DID MOUNT", this.state.name);
 
-			.catch(function(error) {
-				alert(error);
-			});
-	}
-	render() {
-		//   console.log("IN RENDER", this.state.List_of_services);
-		// this.state.List_of_services.map(item => {
+      .catch(function(error) {
+        alert(error);
+      });
+  }
+  render() {
+    //   console.log("IN RENDER", this.state.List_of_services);
+    // this.state.List_of_services.map(item => {
 
-		// });
-		console.log(this.state.List_of_services);
-		if (this.state.List_of_services.length === 0) {
-			return <h2>You have no serive to display </h2>;
-		}
-		return this.state.List_of_services.map(items => {
-			return (
-				<Grid item center xs={4} spacing={10} style={{ marginLeft: 450 }}>
-					<Card key={items._id}>
-						<CardHeader
-							avatar={<Avatar aria-label="recipe">R</Avatar>}
-							title={items.serviceName}
-							//	subheader="September 14, 2016"
-						/>
-						<CardActionArea>
-							<CardMedia
-								component="xuz"
-								alt="Contemplative Reptile"
-								height="140"
-								title="Contemplative Reptile"
-							/>
-							<CardContent>
-								<Typography gutterBottom variant="h5" component="h2">
-									Lizard
-								</Typography>
-								<Typography variant="body2" color="textSecondary" component="p">
-									Lizards are a widespread group of squamate reptiles, with over
-									6,000 species, ranging across all continents except Antarctica
-								</Typography>
-							</CardContent>
-						</CardActionArea>
-						<CardActions>
-							<Button size="large" color="primary">
-								Share
-							</Button>
-						</CardActions>
-					</Card>
-					;
-				</Grid>
-			);
-		});
-	}
+    // });
+    console.log(this.state.List_of_services);
+    if (this.state.List_of_services.length === 0) {
+      return <h2>You have no serive to display </h2>;
+    }
+    return this.state.List_of_services.map(items => {
+      return (
+        <div style={{ flexGrow: 1 }}>
+          {/* <Grid container spacing={3} style={{ marginTop: 5 }}>
+            <Grid item lg={6} xs={6}> */}
+          <Card key={items._id} item lg={6} xs={6}>
+            <CardHeader
+              avatar={<Avatar aria-label="recipe">R</Avatar>}
+              title={items.serviceName}
+              //	subheader="September 14, 2016"
+            />
+            <CardActionArea>
+              <CardMedia
+                component="xuz"
+                alt="Contemplative Reptile"
+                height="140"
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h5">
+                  Price: {items.servicePrice}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {items.serviceDescription}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button
+                size="large"
+                color="primary"
+                variant="contained"
+                ariant="body2"
+                style={{ cursor: "pointer" }}
+              >
+                Book now
+              </Button>
+            </CardActions>
+          </Card>
+          {/* </Grid>
+          </Grid> */}
+        </div>
+      );
+    });
+  }
 }
